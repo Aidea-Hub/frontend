fed47ce7943b30e971f9c48d43795584434d8523
Marcus Tang <txk.marcus@gmail.com>
2023-09-24 15:03:12 +0800

    Add stemming and keyword for searching

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,10 +1,29 @@
-import { Box, Container, Heading, Input, Text } from '@chakra-ui/react'
+import { Box, Button, Container, Heading, Input, Text } from '@chakra-ui/react'
+import keywordExtractor from 'keyword-extractor'
+import { useState } from 'react'
 import { useRecoilValue } from 'recoil'
+import { stemmer } from 'stemmer'
 import Header from '../../components/Head'
 import { themeSelector } from '../../recoil/selectors'
 
 export default function Search() {
   const theme = useRecoilValue(themeSelector)
+
+  const [value, setValue] = useState('')
+  const [keyword, setKeyword] = useState('')
+
+  const handleSubmit = () => {
+    const extractedKeywords = keywordExtractor.extract(value, {
+      language: 'english',
+      remove_digits: true,
+      return_changed_case: true,
+      remove_duplicates: true,
+    })
+    if (extractedKeywords.length > 0) {
+      setKeyword(stemmer(extractedKeywords[0]))
+      console.log(keyword)
+    }
+  }
 
   return (
     <>
@@ -20,7 +39,12 @@ export default function Search() {
           <Text fontSize="lg" fontWeight="semibold" mt={2} mb={2}>
             Find specific ideas
           </Text>
-          <Input></Input>
+          <Input
+            value={value}
+            onChange={event => setValue(event.target.value)}
+            placeholder="Search for ideas"
+          />
+          <Button onClick={handleSubmit}>Search</Button>
         </Box>
       </Container>
     </>
